e8afd33ce9b4138835dedacfbd819b519f33cbae
Khac Man <khacmanle@gmail.com>
2017-12-15 00:28:34 +0700

    Add config, test function

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
+    log() {
+        console.log("Hello");
+    }
   render() {
     return (
       <div className="App row">
         <header className="jumbotron row">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Crypton</h1>
+          <h1 className="App-title" onClick={this.log}>Welcome to Crypton</h1>
             <p>Simple message encrypt-decrypt service</p>
         </header>
       </div>
